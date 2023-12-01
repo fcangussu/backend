d05abfeb8dca3f954999fd02e9b4274db8b995ad
Fernando <fernando.cangussu@telefonica.com>
2023-12-01 15:48:05 -0300

    adicionado setMockedList()
    Signed-off-by: Fernando <fernando.cangussu@telefonica.com>

--- src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
+++ src/test/java/br/com/fiap/api/repository/MensagemRepositoryTest.java
@@ -5,12 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
 import java.util.UUID;
-
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
